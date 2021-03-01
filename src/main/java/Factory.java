f2ece7394e0e8b7c1ae3725538ab89caad0f8c57
IfrimAndrei <andrei_ifrim@yahoo.com>
2021-03-01 10:12:36 +0200

    Versiune 1.1

---
+++ src/main/java/Factory.java
@@ -0,0 +1,8 @@
+public class Factory extends Source{
+    public Factory() {
+    }
+
+    public Factory(String name, int supply) {
+        super(name, supply);
+    }
+}
---
+++ src/main/java/Warehouse.java
@@ -0,0 +1,9 @@
+public class Warehouse extends Source {
+    public Warehouse() {
+    }
+    public Warehouse(String name, int supply) {
+        super(name, supply);
+    }
+
+
+}
